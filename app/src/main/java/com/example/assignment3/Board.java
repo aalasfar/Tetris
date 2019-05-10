da0104b1541413acef1147facd00cb2e3a0705b1
Abdullah Alasfar <aalasfar@uci.edu>
2019-05-09 20:57:04 -0700

    5/9/19 8:56 upddate

---
+++ app/src/main/java/com/example/assignment3/Board.java
@@ -0,0 +1,21 @@
+package com.example.assignment3;
+
+import java.awt.Color;
+import java.awt.Dimensions;
+import java.awt.Graphics;
+import java.util.Timer;
+
+public class Board extends JPanel implements ActionListener {
+
+    private static final int BOARD_WIDTH = 10;
+    private static final int BOARD_HEIGHT = 16;
+    private Timer timer;
+    private boolean isFallingDone = false;
+    private boolean isStarted = false;
+    private boolean isPaused = false;
+    private int numberLinesRemoved = 0;
+    private int currentX = 0;
+    private int currentY = 0;
+    private JLabel statusBar;
+
+}
---
+++ app/src/main/java/com/example/assignment3/Shape.java
@@ -0,0 +1,8 @@
+package com.example.assignment3;
+
+import java.util.Random;
+
+public class Shape {
+    //public static int Tetrominoe()
+
+}
